08d7da62612ecdb531f1e96f6bd9a6ff940eac80
frankyle <magaifrank@gmail.com>
2024-08-19 20:08:28 +0300

    User-register-login-MGICANDLES-CRUD

---
+++ Backend/mgicandles/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import MgiCandles
+
+class MgiCandlesAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'trade_signal', 'is_active', 'signal_candle', 'hour_candle', 'created_at', 'currency_pair', 'session',
+        'five_min_order_block', 'flip_four_hour_candle', 'four_hour_break_of_structure', 'five_min_break_of_structure',
+        'change_color_ut_alert'
+    )
+    search_fields = (
+        'user__username', 'trade_signal',
+    )
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'created_at')
+        }),
+        ('Trade Details', {
+            'fields': ('trade_signal', 'is_active', 'currency_pair', 'session')
+        }),
+        ('Candles', {
+            'fields': ('signal_candle', 'hour_candle')
+        }),
+        ('Additional Details', {
+            'fields': ('five_min_order_block', 'flip_four_hour_candle', 'four_hour_break_of_structure', 'five_min_break_of_structure', 'change_color_ut_alert')
+        }),
+    )
+
+admin.site.register(MgiCandles, MgiCandlesAdmin)
---
+++ Backend/mgicandles/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.conf import settings
+
+class MgiCandles(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    # Trade Details
+    trade_signal = models.CharField(max_length=4, choices=(("buy", "Buy"), ("sell", "Sell")), null=True, blank=True)
+    is_active = models.BooleanField(default=False, null=True, blank=True)
+    
+    # Existing fields
+    signal_candle = models.ImageField(upload_to='signal_candles/', null=True, blank=True)
+    hour_candle = models.ImageField(upload_to='hour_candles/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # New fields
+    candle_pattern = models.CharField(
+        max_length=20,
+        choices=(
+            ("engulfing", "Engulfing Candle"), 
+            ("small", "Small Candle"),
+            ("pinbar", "Pin bar Candle"),
+        ),
+        blank=True,
+        null=True
+    )
+    fibonacci_level = models.CharField(
+        max_length=5,
+        choices=(
+            ("38.2%", "38.2%"),
+            ("50%", "50%"),
+            ("61.8%", "61.8%"),
+            ("78.6%", "78.6%"),
+        ),
+        blank=True,
+        null=True
+    )
+    session = models.CharField(
+        max_length=15,
+        choices=(
+            ("london", "London Session"),
+            ("newyork", "New York Session"),
+        ),
+        blank=True,
+        null=True
+    )
+    currency_pair = models.CharField(max_length=6, null=True, blank=True)
+
+     # New fields
+    five_min_order_block = models.BooleanField(default=False, null=True, blank=True)
+    flip_four_hour_candle = models.BooleanField(default=False, null=True, blank=True)
+    four_hour_break_of_structure = models.BooleanField(default=False, null=True, blank=True)
+    five_min_break_of_structure = models.BooleanField(default=False, null=True, blank=True)
+    change_color_ut_alert = models.BooleanField(default=False, null=True, blank=True)
+    
+    class Meta:
+        verbose_name = "Mgi candle"
+        verbose_name_plural = "Mgi candles"
